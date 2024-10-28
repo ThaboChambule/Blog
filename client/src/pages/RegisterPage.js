b66a1d6e0869124e351e4f5bf4764f6cb50db6fc
Thabo <thabochambule1@gmail.com>
2024-10-28 14:08:22 +0200

    configure form

--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -1,10 +1,29 @@
-export default function RegisterPage(){
-return(
-    <form className="register">
-        <h1>Register</h1>
-    <input type="text" placeholder="username"/>
-    <input type="password" placeholder="password"/>
-    <button>Register</button>
-</form>
-)
+import {useState} from 'react';
+
+export default function RegisterPage() {
+    const [username,setUsername]=useState('')
+    const [password,setPassword]=useState('')
+    function register(ev){
+        ev.preventDefault() //it prevents html page from redirecting
+        fetch('http://localhost:4000',{
+            method:'POST',
+            body: JSON.stringify({username,password})
+        })
+    }
+
+  return (
+    <form className="register" onSubmit={register}>
+      <h1>Register</h1>
+      <input type="text" 
+      placeholder="username" 
+      value={username} 
+      onChange={ev => setUsername(ev.target.value)} />
+      <input type="password" 
+      placeholder="password"
+      value={password}
+      onchange={ev => setPassword(ev.target.value)}
+      />
+      <button>Register</button>
+    </form>
+  );
 }

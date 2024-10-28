286caead78f504f52e7b840b3bd0277b03c34cbe
Thabo <thabochambule1@gmail.com>
2024-10-28 11:29:57 +0200

    create react app

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <header>
+      <a href="#" className="logo">MyBlog</a>
+      <a href="">Login</a>
+      <a href="">Register</a>
+
+      </header>
+
+
+
+
+
+
+
+
+    </main>
+  );
+}
+
+export default App;

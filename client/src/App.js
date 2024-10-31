a30bef6760aae9aa8bf2080475ef05e77ac9d43d
Thabo <thabochambule1@gmail.com>
2024-11-01 00:20:59 +0200

    Add new createpost and form

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import IndexPage from "./pages/IndexPage";
 import LoginPage from "./pages/LoginPage"
 import RegisterPage from "./pages/RegisterPage";
 import { UserContextProvider } from "./UserContext";
+import CreatePost from "./pages/CreatePost";
 
 function App() {
   return (
@@ -14,8 +15,9 @@ function App() {
        <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<IndexPage />} />
-        <Route path={"/login"} element={<LoginPage/>} />
-        <Route path={"/register"} element={<RegisterPage/>}/>
+        <Route path="/login" element={<LoginPage/>} />
+        <Route path="/register" element={<RegisterPage/>}/>
+        <Route path="/create" element={<CreatePost/>}/>
       </Route>
     </Routes>
     </UserContextProvider>
